e034a6ac9e473e8c4801e78401daf4ee65eec1f4
Your Name <you@example.com>
2021-04-15 12:39:19 +0100

    #comment code for tracks page implemented

--- src/test/java/qa/choonz/pages/TracksPage.java
+++ src/test/java/qa/choonz/pages/TracksPage.java
@@ -1,5 +1,56 @@
 package qa.choonz.pages;
 
-public class TracksPage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
+public class TracksPage {
+	public static final String TracksURL = "http://http://localhost:8082/tracks";
+
+	// Album-name link
+	@FindBy(xpath = "/html/body/section/div/div/div[1]/div[4]/a")
+	private WebElement albumName;
+
+	public void clickAlbumName() {
+		albumName.click();
+	}
+
+	// Genre-name link
+	@FindBy(xpath = "/html/body/section/div/div/div[1]/div[5]/a")
+	private WebElement genreName;
+
+	public void clickGenreName() {
+		genreName.click();
+	}
+	
+	// Edit Track
+	
+	@FindBy(xpath = "/html/body/section/div/div/div[3]/div[1]/button")
+	private WebElement editTrack;
+
+	
+	
+	public void clickEditTrack() {
+		editTrack.click();
+	}
+	
+	@FindBy(xpath = "/html/body/section/div/div/div[3]/div[2]/button")
+	private WebElement deleteTrack;
+	
+	public void clickDeleteTrack() {
+		deleteTrack.click();
+	}
+	
+	@FindBy(xpath = "/html/body/div[4]/div/div/div[3]/button[1]")
+	private WebElement closeDeleteTrack;
+	
+	public void clickCloseDeleteTrack() {
+		closeDeleteTrack.click();
+	}
+	
+	@FindBy(xpath = "/html/body/div[4]/div/div/div[3]/button[2]")
+	private WebElement dDeleteTrack;
+	
+	public void clickdDeleteTrack() {
+		dDeleteTrack.click();
+	}
 }
